97c57b70534480c6768fba0255882280a0c5e910
Adrian <adriancoceres.93@gmail.com>
2025-04-24 15:11:00 -0300

    form

--- react-consulta/src/components/Formulario.jsx
+++ react-consulta/src/components/Formulario.jsx
@@ -1,5 +1,19 @@
-import React, {useState} from 'react;'
-
-export default function Formulario(){
-    
+import React, { useState } from 'react';
+export default function Formulario() {
+    const [nombre, setNombre] = useState('');
+    function manejarEnvio(evento) {
+        evento.preventDefault();
+        alert(`Formulario enviado por: ${nombre}`);
+    }
+    return (
+        <form onSubmit={manejarEnvio}>
+            <input
+                type="text"
+                value={nombre}
+                onChange={(e) => setNombre(e.target.value)}
+                placeholder="Ingresa tu nombre"
+            />
+            <button type="submit">Enviar</button>
+        </form>
+    );
 }
