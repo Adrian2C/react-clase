ec59d9a9fd7e0d9ab3bf830a795ea1d2dc8fb390
Adrian2C <adriancoceres.93@gmail.com>
2025-05-15 19:53:33 -0300

    cartContext

---
+++ Ejemplo de pre entrega sin terminar/src/context/CartContext.jsx
@@ -0,0 +1,64 @@
+import { createContext, useEffect, useState } from 'react'
+
+export const CartContext = createContext()
+
+export const CartProvider = ({ children }) => {
+    /* aca se pueden poner todos los elementos que estaban en el app.jsx, para que no llenene la pagina */
+
+    const [cart, setCart] = useState([])
+    const [productos, setProductos] = useState([])
+    const [cargando, setCargando] = useState(true)
+    const [error, setError] = useState(false)
+    const [isAuthenticated, setIsAuth] = useState(false)
+
+    useEffect(() => {
+        fetch('/data/data.json')
+            .then(respuesta => respuesta.json())
+            .then(datos => {
+                setTimeout(() => {
+                    setProductos(datos)
+                    setCargando(false)
+                }, 2000)
+            })
+            .catch(error => {
+                console.log('Error', error)
+                setCargando(false)
+                setError(true)
+            })
+
+    }, [])
+
+    const handleAddToCart = (product) => {
+
+        const productInCart = cart.find((item) => item.id === product.id);
+        if (productInCart) {
+
+            setCart(cart.map((item) => item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item));
+        } else {
+            setCart([...cart, { ...product, quantity: 1 }]);
+        }
+    };
+
+    const handleDeleteFromCart = (product) => {
+        setCart(prevCart => {
+            return prevCart.map(item => {
+                if (item.id === product.id) {
+                    if (item.quantity > 1) {
+                        return { ...item, quantity: item.quantity - 1 };
+                    } else {
+                        return null; // Si quantity es 1, marcamos para eliminar
+                    }
+                } else {
+                    return item; // Si no es el producto, lo dejamos igual
+                }
+            }).filter(item => item !== null); // Quitamos los productos nulos
+        });
+    };
+
+    /* todos estos elementos que se pasan a esta carpeta, ahora tienen que ponerse en el value, todos esos que van a afetar ela app */
+    return (
+        <CartContext.Provider value={{ cart, productos, cargando, error, handleAddToCart, handleDeleteFromCart, isAuthenticated }}>
+            {children}
+        </CartContext.Provider>
+    )
+}
